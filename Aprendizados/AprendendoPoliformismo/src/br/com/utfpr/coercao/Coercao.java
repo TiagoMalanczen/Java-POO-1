ec9d7f5463ae70b63503010e8f7c8ad5bace0d3c
Tiago Malanczen <malanczen13@gmail.com>
2026-06-17 16:17:45 -0300

    feat : apresenta o basico de corcao para tipos primitivos

---
+++ Aprendizados/AprendendoPoliformismo/src/br/com/utfpr/coercao/Coercao.java
@@ -0,0 +1,22 @@
+package br.com.utfpr.coercao;
+
+public class Coercao {
+
+	public static void main(String[] args) {
+		
+		//Corcao explicita
+		float w = (float) 12.34;
+		int x = (int) 56.78;
+		
+		//
+		double y = 10;
+		int z = 'z';
+		
+		System.out.println(w);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		
+	}
+}
