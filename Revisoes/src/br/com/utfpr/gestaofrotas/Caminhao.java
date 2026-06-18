f82599dadf4bf47afd762f1646f9e570bb2aa78c
Tiago Malanczen <malanczen13@gmail.com>
2026-06-18 15:11:51 -0300

    feat : adiciona metodos sobre sobrecarga

--- Revisoes/src/br/com/utfpr/gestaofrotas/Caminhao.java
+++ Revisoes/src/br/com/utfpr/gestaofrotas/Caminhao.java
@@ -1,5 +1,23 @@
 package br.com.utfpr.gestaofrotas;
 
-public class Caminhao {
+public class Caminhao extends Veiculo{
 
+	private double pesoCarga;//em T
+
+	public Caminhao(int id, String modelo, double capacidadeTanque, double consumoMedio, double pesoCarga) {
+		super(id, modelo, capacidadeTanque, consumoMedio);
+		this.pesoCarga = pesoCarga;
+	}
+	
+	@Override
+	public void imprimirDados() {
+		super.imprimirDados();
+		System.out.println("Peso carga = " + this.pesoCarga);
+	}
+	@Override
+	public double calcularAutonomiaMaxima() {
+		int pesoCargaInt = (int)this.pesoCarga;
+		double perca = pesoCargaInt*0.05 ;
+		return this.capacidadeTanque*this.consumoMedio - perca;
+	}
 }
--- Revisoes/src/br/com/utfpr/gestaofrotas/Veiculo.java
+++ Revisoes/src/br/com/utfpr/gestaofrotas/Veiculo.java
@@ -1,5 +1,44 @@
 package br.com.utfpr.gestaofrotas;
 
-public class Veiculo {
+public abstract class Veiculo {
 
+	protected int id;
+	protected String modelo;
+	protected double capacidadeTanque;
+	protected double consumoMedio;
+	
+	public Veiculo(int id, String modelo, double capacidadeTanque, double consumoMedio) {
+		super();
+		this.id = id;
+		this.modelo = modelo;
+		this.capacidadeTanque = capacidadeTanque;
+		this.consumoMedio = consumoMedio;
+	}
+	
+	public abstract double calcularAutonomiaMaxima();
+	
+	public void imprimirDados() {
+		System.out.println("Id = " + this.id);
+		System.out.println("Modelo= " + this.modelo);
+		System.out.println("Capacidade tanque = " + this.capacidadeTanque);
+		System.out.println("Consumo medio = " + this.consumoMedio);
+	}
+
+	
+	public void abastecer(double litros) {
+		System.out.println("Abastecendo com " + litros + " o automovel");
+	}
+	public void abastecer(double quantiade, String tipoEnergia) {
+		if(tipoEnergia.contentEquals("Eletrecidade")) {
+			System.out.println("Baterria recarregada " + quantiade + "kWh");
+		}
+		else {
+			System.out.println("Recarregado com sucesso com combustivel fossil");
+		}
+	}
+	
+	public void abastecer(double litros, boolean cupomDesconto) {
+		if()
+	}
+	
 }
