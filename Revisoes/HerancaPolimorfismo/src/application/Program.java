ed00906a745d1b8175e087a9f57b7f6a0ad1ab67
Tiago Malanczen <malanczen13@gmail.com>
2026-07-28 15:54:51 -0300

    feat : adiciona revisao basica sobre polimorfismo

--- Revisoes/HerancaPolimorfismo/src/application/Program.java
+++ Revisoes/HerancaPolimorfismo/src/application/Program.java
@@ -1,51 +1,20 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Account acc = new Account(1001, "Alex", 1000.0);
-		BusinessAccount bacc = new BusinessAccount(1001, "Maria", 1000.0, 0.1);
-		SavingsAccount sacc = new SavingsAccount(1001, "Mario", 1000.0, 0.1);
 		
-		// UPCASTING
-		Account acc1 = bacc;
-		acc1.getBalance();
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Anna", 10.0, 1200.0);
-
-		// DOWNCASTING
-		BusinessAccount acc4 = (BusinessAccount) acc2;
-		acc4.loan(20.00);
-
-		// BusinessAccount acc5 = (BusinessAccount) acc3;
-
-		if (acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount) acc3;
-			acc5.loan(300.00);
-			System.out.println("Loan");
-		}
-
-		if (acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount) acc3;
-			acc5.updateBalance();
-			System.out.println("Update!");
-		}
-		System.out.println();
+		Account x = new Account(1020, "Alex" , 1000.00);
+		Account y = new SavingsAccount(1023, "Maria" , 1000.00, 0.01);
 		
-		acc.withdraw(200.0);
-		System.out.println(acc.getBalance());
-		
-		sacc.withdraw(200.0);
-		System.out.println(sacc.getBalance());
-		
-		bacc.withdraw(200.0);
-		System.out.println(bacc.getBalance());
-
-		
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+	
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 	}
+	
 }
