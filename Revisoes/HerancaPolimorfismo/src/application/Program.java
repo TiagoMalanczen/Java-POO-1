72bf104fa4cb3163bdada6d41267df9190a6dc4b
Tiago Malanczen <malanczen13@gmail.com>
2026-07-29 10:19:48 -0300

    feat : adiciona revisao basica sobre classes abstratas

--- Revisoes/HerancaPolimorfismo/src/application/Program.java
+++ Revisoes/HerancaPolimorfismo/src/application/Program.java
@@ -1,20 +1,40 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import entities.Account;
+import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Account x = new Account(1020, "Alex" , 1000.00);
-		Account y = new SavingsAccount(1023, "Maria" , 1000.00, 0.01);
+		List<Account> list = new ArrayList<>();
 		
-		x.withdraw(50.0);
-		y.withdraw(50.0);
-	
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
+		Account acc1 = new SavingsAccount(1002, "Maria" , 1000.00, 0.1);
+		Account acc2 = new BusinessAccount(1003, "Marcos" , 1000.00, 500.0);
+
+		list.add(acc1);
+		list.add(acc2);
+		
+		double sum = 0.0;
+		
+		for(Account ac : list) {
+			sum += ac.getBalance();
+		}
+		System.out.println(sum);
+		
+		for(Account ac : list) {
+			ac.deposit(10);
+		}
+		System.out.println("Atualizado");
+		for(Account ac : list) {
+			System.out.println(ac.getBalance());
+		}
+		
+
 	}
 	
 }
--- Revisoes/HerancaPolimorfismo/src/entities/Account.java
+++ Revisoes/HerancaPolimorfismo/src/entities/Account.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Account {
+public abstract class Account {
 
 	protected Integer number;
 	protected String holder;
