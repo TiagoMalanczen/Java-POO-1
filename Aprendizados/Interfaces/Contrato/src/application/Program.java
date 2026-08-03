b6310bbfcbfaa2b7f5fdd1f96615871b29704873
Tiago Malanczen <malanczen13@gmail.com>
2026-08-03 11:57:07 -0300

    feat: adiciona outro exemplo utilizando interface

---
+++ Aprendizados/Interfaces/Contrato/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Contract;
+import entities.Installment;
+import services.ContractService;
+import services.PayPalServices;
+
+public interface Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do contrato");
+		System.out.println("Numero: ");
+		int number = sc.nextInt();
+		System.out.println("Data:");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.println("Valor do contrato");
+		double totalValue = sc.nextDouble();
+		
+		Contract obj = new Contract(number, date, totalValue);
+		
+		System.out.println("Entre com o numero de parcelas: ");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(new PayPalServices());
+		
+		contractService.processContract(obj, n);
+		
+		System.out.println("Parcelas:");
+
+		for(Installment installment : obj.getInstallments()) {
+			 System.out.println(installment.toString());
+		}
+		
+		sc.close();
+	}
+	
+}
