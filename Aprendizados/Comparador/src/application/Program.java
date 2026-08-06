7c4da113dca8beaf584dc31f2a977a04af9e5224
Tiago Malanczen <malanczen13@gmail.com>
2026-08-06 15:15:15 -0300

    refactor: troca o metodo de comparacao ultilizando class anônima

--- Aprendizados/Comparador/src/application/Program.java
+++ Aprendizados/Comparador/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -11,12 +12,18 @@ public class Program {
 	public static void main(String[] args) {
 		
 		List<Product> list = new ArrayList<>();
-	
+	 
 		list.add(new Product("C", 150.00));
 		list.add(new Product("A", 1000.00));
 		list.add(new Product("B", 700.00));
 		
-		list.sort(new MyComparator());
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+		list.sort(comp);
 		
 		for(Product p: list) {
 			System.out.println(p);
